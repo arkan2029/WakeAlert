123730dda857915fd5fe63d334e86fccbfeefb90
arkan2029 <arnav.kanchi@gmail.com>
2026-01-19 01:06:17 -0600

    Initial Commit

---
+++ WakeAlert/WakeAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeAlertApp.swift
+//  WakeAlert
+//
+//  Created by Kanchi  on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WakeAlertTests/WakeAlertTests.swift
@@ -0,0 +1,17 @@
+//
+//  WakeAlertTests.swift
+//  WakeAlertTests
+//
+//  Created by Kanchi  on 1/19/26.
+//
+
+import Testing
+@testable import WakeAlert
+
+struct WakeAlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
